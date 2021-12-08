95c3db1abcbd9862ba774444655b364f0fda2dce
Akash Raj <95422208+AkashRaj23@users.noreply.github.com>
2021-12-08 16:05:24 +0530

    Add files via upload

---
+++ Word Counter/script.js
@@ -0,0 +1,13 @@
+let textbox = document.getElementById("text");
+    
+    textbox.addEventListener("input", function () {
+      let text = this.value;
+      let char = text.length;
+      document.getElementById("char").innerHTML = char;
+      text =text.trim();
+      let word =text.split(" ");
+      let cleanList=word.filter(function(elm){
+        return elm!="";
+      })
+      document.getElementById("word").innerHTML=cleanList.length;
+    });
